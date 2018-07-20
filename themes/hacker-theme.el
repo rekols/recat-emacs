e5d85049132f0486b69e091ce5a59f0a990de137
rekols <rekols@foxmail.com>
2018-07-20 17:51:30 +0800

    hacker-theme: optimize native line numbers style

--- themes/hacker-theme.el
+++ themes/hacker-theme.el
@@ -15,24 +15,29 @@
 '(tabbar-background-color "#24292E"))
 
 (custom-set-faces
-   '(default ((t (:background "#24292E" :foreground "green"))))  ; 全局背景00FF00
+   '(default ((t (:background "#24292E" :foreground "#00FF00"))))  ; 全局背景
    '(hl-line ((t (:background "#2F3336"))))                        ; 高亮当前行
-   '(region ((t (:background "green4" :foreground "#FFFFFF"))))    ; 选中区域
-   '(linum ((t (:background "#24292E" :foreground "#586E75"))))    ; 行数栏
+   '(region ((t (:background "green4" :foreground "black"))))    ; 选中区域
    '(cursor ((t (:background "#717171"))))                         ; 光标颜色
 
-   ; mode-line
+   ;; linum
+   '(linum ((t (:background "#24292E" :foreground "#586E75"))))
+   ;; native line numbers (emacs version >= 26)
+   '(line-number ((t (:background "#24292E" :foreground "#586E75"))))
+   '(line-number-current-line ((t (:background "#24292E" :foreground "#779097"))))
+
+   ;; mode-line
    '(mode-line ((t (:background "#474C51" :foreground "white" :box(:color "#474C51" :line-width 1)))))
    '(mode-line-inactive ((t (:background "#2F3338" :foreground "white" :box(:color "#2F3338" :line-width 1)))))
    `(mode-line-buffer-id ((t (:weight bold))))
    `(mode-line-emphasis ((t (:weight bold))))
 
-   ; tabbar
+   ;; tabbar
    '(tabbar-default ((t (:inherit variable-pitch :background "#24292E" :foreground "green" :height 1.1 :family "Droid Sans Mono"))))
    '(tabbar-selected ((t (:inherit tabbar-default :box (:line-width 1 :color "#474C51")))))
    '(tabbar-unselected ((t (:inherit tabbar-default :background "#24292E" :foreground "#969696" :box (:line-width 1 :color "#474C51")))))
 
-   ; dired
+   ;; dired
    '(diredp-dir-heading ((t (:background "#24292E" :foreground "Gold"))))
 )
 
