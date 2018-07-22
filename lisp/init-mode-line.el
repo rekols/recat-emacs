6474cc14192f2a3cfae1744f99debf645b0b4839
rekols <rekols@foxmail.com>
2018-07-23 00:07:14 +0800

    remove mode-line line number

--- lisp/init-mode-line.el
+++ lisp/init-mode-line.el
@@ -1,9 +1,8 @@
 (setq-default mode-line-format
-              (list '(:propertize " %l " face (:weight bold))
+              (list ;'(:propertize " %l " face (:weight bold))
                     '(:propertize " %b ") ;; file name
-                    ;; '(:propertize " %I ") ;; file size
                     '(:propertize " %m ")
-                    '(:eval (propertize (format-time-string " %p·%H:%M ") 'help-echo (format-time-string "%F %a")))
+                    '(:eval (propertize (format-time-string " %p/%H:%M ") 'help-echo (format-time-string "%F %a")))
                     ))
 
 (provide 'init-mode-line)
